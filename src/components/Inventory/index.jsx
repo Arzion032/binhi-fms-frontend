62ddbdc433f9eb78da39f82641c3e513271771e7
Rhon Villamarin <rhonv316@gmail.com>
2025-05-30 01:24:45 +0800

    all

---
+++ src/components/Inventory/index.jsx
@@ -0,0 +1,18 @@
+// Auto-generated index.jsx for exporting all components
+
+export { default as AddEquipmentModal } from './AddEquipmentModal';
+export { default as EditEquipmentModal } from './EditEquipmentModal';
+export { default as DeleteConfirmationModal } from './DeleteConfirmationModal';
+export { default as DisregardModal } from './DisregardModal';
+export { default as ReturnModal } from './ReturnModal';
+export { default as SuccessModal } from './SuccessModal';
+export { default as EquipmentPage } from './EquipmentPage';
+export { default as TabsHeader } from './TabsHeader';
+export { default as EquipmentTab } from './EquipmentTab';
+export { default as RentHistoryTab } from './RentHistoryTab';
+export { default as EquipmentCards } from './EquipmentCards';
+export { default as EquipmentTable } from './EquipmentTable';
+export { default as RentTable } from './RentTable';
+export { default as EquipmentModals } from './EquipmentModals';
+export { default as RentModals } from './RentModals';
+
