92f5d8e4c3ba688112c1800bc973e464a6beb5dd
Rhon Villamarin <rhonv316@gmail.com>
2025-05-31 22:25:01 +0800

    sign Up

--- src/components/SignUp/Card.jsx
+++
@@ -1,7 +0,0 @@
-export const Card = ({ children, className }) => {
-    return (
-      <div className={`p-6 rounded-2xl bg-white shadow-md ${className}`}>
-        {children}
-      </div>
-    );
-  };
